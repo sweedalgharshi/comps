2d3f3e67ba3076a051c1ceab9d9e3f309a38ca60
sweedalgharshi <sweedalgracius@gmail.com>
2023-01-10 17:40:22 +0530

    Added proptypes and tailwindCss library

--- src/Button.js
+++ src/Button.js
@@ -1,5 +1,33 @@
-function Button({ children }) {
+import PropTypes from 'prop-types';
+
+function Button({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outlined,
+  rounded,
+}) {
   return <button>{children}</button>;
 }
+
+Button.propTypes = {
+  checkVariationValue: ({ primary, secondary, warning, danger, success }) => {
+    const count =
+      Number(!!primary) +
+      Number(!!secondary) +
+      Number(!!warning) +
+      Number(!!danger) +
+      Number(!!success);
+
+    if (count > 1) {
+      return new Error(
+        'Only one of primary,secondary, warning, danger, success'
+      );
+    }
+  },
+};
 
 export default Button;

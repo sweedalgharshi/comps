8214e5b914f558d8006c3a71c47c170f4c206f3a
sweedalgharshi <sweedalgracius@gmail.com>
2023-01-18 12:35:38 +0530

    modal-part-2 implementation and fixing bugs

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -10,12 +10,30 @@ function ModalPage() {
     setShowModal(true);
   };
 
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const actionBar = (
+    <div>
+      <Button primary onClick={handleClose}>
+        I Accept
+      </Button>
+    </div>
+  );
+
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>Here is an important agreement for you to accept</p>
+    </Modal>
+  );
+
   return (
     <div>
       <Button onClick={handleClick} primary>
         Open Modal
       </Button>
-      {showModal && <Modal />}
+      {showModal && modal}
     </div>
   );
 }

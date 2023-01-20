ff28f32ae5e78124ddb5a74a16b50a811cfb10cc
sweedalgharshi <sweedalgracius@gmail.com>
2023-01-20 16:27:47 +0530

    adding new state update reducer part-3

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -2,22 +2,57 @@ import { useReducer } from 'react';
 import Button from '../components/Button';
 import Panel from '../components/Panel';
 
+const INCREMENT_COUNT = 'increment';
+const DECREMENT_COUNT = 'decrement';
+const SET_VALUE_TO_ADD = 'set-value-to-add';
+const ADD_VALUE_TO_COUNT = 'add-value-to-count';
+
 const reducer = (state, action) => {
-  if (action.type === 'increment') {
-    return {
-      ...state,
-      count: state.count + 1,
-    };
-  }
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
 
-  if (action.type === 'change-value-to-add') {
-    return {
-      ...state,
-      valueToAdd: action.payload,
-    };
-  }
+    case DECREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
 
-  return state;
+    case SET_VALUE_TO_ADD:
+      return {
+        ...state,
+        valueToAdd: action.payload,
+      };
+
+    case ADD_VALUE_TO_COUNT:
+      return {
+        ...state,
+        count: state.count + state.valueToAdd,
+        valueToAdd: 0,
+      };
+
+    default:
+      // throw new Error('unexpected action type', +action.type);
+      return state;
+  }
+  // if (action.type === INCREMENT_COUNT) {
+  //   return {
+  //     ...state,
+  //     count: state.count + 1,
+  //   };
+  // }
+
+  // if (action.type === SET_VALUE_TO_ADD) {
+  //   return {
+  //     ...state,
+  //     valueToAdd: action.payload,
+  //   };
+  // }
+
+  // return state;
 };
 
 function CounterPage({ initialCount }) {
@@ -31,19 +66,22 @@ function CounterPage({ initialCount }) {
   const increment = () => {
     // setCount(count + 1);
     dispatch({
-      type: 'increment',
+      type: INCREMENT_COUNT,
     });
   };
 
   const decrement = () => {
     // setCount(count - 1);
+    dispatch({
+      type: DECREMENT_COUNT,
+    });
   };
 
   const handleChange = (e) => {
     const value = parseInt(e.target.value) || 0;
     // setValueToAdd(value);
     dispatch({
-      type: 'change-value-to-add',
+      type: SET_VALUE_TO_ADD,
       payload: value,
     });
   };
@@ -52,6 +90,10 @@ function CounterPage({ initialCount }) {
     e.preventDefault();
     // setCount(count + valueToAdd);
     // setValueToAdd(0);
+
+    dispatch({
+      type: ADD_VALUE_TO_COUNT,
+    });
   };
 
   console.log(state);
